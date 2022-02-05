69b8aaf80b44e2907ec188ff03eed2fa801565d8
wanysmoris <68104734+wanysmoris@users.noreply.github.com>
2022-02-06 00:54:08 +0200

    Add files via upload
    d

---
+++ webpack.dev.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+	entry: "./src/client/index.js",
+	mode: "development",
+	devtool: "source-map",
+	stats: "verbose",
+	output: {
+		libraryTarget: "var",
+		library: "Client",
+		path: path.resolve(__dirname, "dist"),
+	},
+	node: {
+		fs: "empty",
+	},
+	module: {
+		rules: [
+			{
+				test: "/.js$/",
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|ttf)$/,
+				loader: "file-loader",
+				options: {
+					outputPath: "assets",
+					name: "[name].[ext]",
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html",
+		}),
+		new CleanWebpackPlugin({
+			// Simulate the removal of files
+			dry: true,
+			// Write Logs to Console
+			verbose: true,
+			// Automatically remove all unused webpack assets on rebuild
+			cleanStaleWebpackAssets: true,
+			protectWebpackAssets: false,
+		}),
+		new MiniCssExtractPlugin({ filename: "[name].css" }),
+		new WorkboxPlugin.GenerateSW(),
+	],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+	entry: "./src/client/index.js",
+	mode: "production",
+	output: {
+		libraryTarget: "var",
+		library: "Client",
+	},
+	node: {
+		fs: "empty",
+	},
+	module: {
+		rules: [
+			{
+				test: "/.js$/",
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|ttf)$/,
+				loader: "file-loader",
+				options: {
+					outputPath: "assets",
+					name: "[name].[ext]",
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "index.html",
+		}),
+		new WorkboxPlugin.GenerateSW(),
+		new MiniCssExtractPlugin({ filename: "[name].css" }),
+	],
+};
